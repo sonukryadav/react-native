b44166fba6a81de72fa3ade1ebe59549aee9f2eb
sonukryadav <sonukryadav56@gmail.com>
2023-02-20 00:39:37 +0530

    updated validation

--- w1/Assignment/create-a-sign-in-page/App.js
+++ w1/Assignment/create-a-sign-in-page/App.js
@@ -1,21 +1,22 @@
 import * as React from 'react';
-import { 
-  Text, View, StyleSheet, Platform,SafeAreaView, ScrollView,
-  TextInput,Pressable, Alert
+import {
+  Text, View, StyleSheet, Platform, SafeAreaView, ScrollView,
+  TextInput, Pressable, Alert
 } from 'react-native';
 
 
-const Pressable1 = ({title, bg})=>{
-  return(
+const Pressable1 = ({ title, bg }) => {
+  return (
     <Pressable
-    onPress={()=>Alert.alert(title)}
-    style={({pressed})=>[
-      {opacity:pressed ? 0.2 : 1,
-      marginTop:30
-      }
-    ]}
+      onPress={() => Alert.alert(title)}
+      style={({ pressed }) => [
+        {
+          opacity: pressed ? 0.2 : 1,
+          marginTop: 30
+        }
+      ]}
     >
-    {({pressed})=>(<Text style={{...styles.textPressable,backgroundColor:bg}}>{title}</Text>)}
+      {({ pressed }) => (<Text style={{ ...styles.textPressable, backgroundColor: bg }}>{title}</Text>)}
     </Pressable>
   );
 }
@@ -31,79 +32,94 @@ const Pressable1 = ({title, bg})=>{
 export default function App() {
 
   const [input, setInput] = React.useState({
-    email:"",
-    password:""
+    email: "",
+    password: ""
   })
-  const[active, setActive] = React.useState(true);
+  const [active, setActive] = React.useState(true);
+  const [emailV, setEmailV] = React.useState(true);
   const invalid = `Your password is weak, it should have : \n- password should be of 8 characters length \n- password should contain\n- uppercase letter\n- lowercase letter\n- number\n- special character`;
 
-  const inputs =(name, value)=>{
-    setInput({...input, [name] : value});
+  const inputs = (name, value) => {
+    setInput({ ...input, [name]: value });
   }
 
-  React.useEffect(()=>{
+  React.useEffect(() => {
     let password = input.password;
 
     let length = password.length;
-    let upperCharLength = (()=>{
+    let upperCharLength = (() => {
       let matches = password.match(/[A-Z]/g);
       return matches ? matches.length : 0;
     })();
-    let lowerCharLength = (()=>{
+    let lowerCharLength = (() => {
       let matches = password.match(/[a-z]/g);
       return matches ? matches.length : 0;
     })();
-    let digitLength = (()=>{
+    let digitLength = (() => {
       let matches = password.match(/\d/g);
       return matches ? matches.length : 0;
     })();
 
-    let specialCharLength = (()=>{
+    let specialCharLength = (() => {
       let matches = password.match(/[^\w\s]/g);
       return matches ? matches.length : 0;
     })();
 
-    if(length && upperCharLength  && lowerCharLength && digitLength  && specialCharLength ){
+    let emailValidation = (function validateEmail(email) {
+      const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      return regex.test(email);
+    })(input.email);
+
+    if (emailValidation) {
+      setEmailV(false)
+    } else {
+      setEmailV(true);
+    }
+
+
+    if (length >= 8 && upperCharLength && lowerCharLength && digitLength && specialCharLength) {
       setActive(false);
-    }else{
+    } else {
       setActive(true)
     }
     // console.log(upperCharLength, lowerCharLength, digitLength, specialCharLength);
-  },[input]);
+  }, [input]);
 
   return (
     <SafeAreaView style={styles.container}>
-    <ScrollView>
-    <Text style={styles.text1}>Login Page</Text>
-    {/* <Text>Email : {input.email} {`\n`} Password : {input.password}</Text> */}
-    <TextInput
-    style={styles.input1}
-    placeholder="Enter Email"
-    value = {input.email}
-    onChangeText={(email)=>inputs("email", email)}
-    keyboardType="email-address"
-    />
-    <TextInput
-    style={styles.input1}
-    placeholder="Enter Password"
-    value = {input.password}
-    onChangeText={(password)=>inputs("password", password)}
-    secureTextEntry={true}
-    />
-    <Text style={{fontSize:14, marginHorizontal:20, marginVertical:-15, marginBottom:20}}>{active && invalid}</Text>
-    <Pressable
-    onPress={()=>Alert.alert("LOGIN clicked")}
-    style={({pressed})=>[
-      {opacity:pressed ? 0.2 : 1,
-      }
-    ]}
-    disabled={active}
-    >
-    {({pressed})=>(<Text style={{...styles.textPressable,backgroundColor:!active ? "green":"grey"}}>LOGIN</Text>)}
-    </Pressable>
-    <Pressable1 title={"LOGIN WITH GOOGLE"} bg={"coral"}/>
-    <Pressable1 title={"LOGIN WITH FACEBOOK"} bg={"lightblue"}/>
-    </ScrollView>
+      <ScrollView>
+        <Text style={styles.text1}>Login Page</Text>
+        {/*<Text>Email : {input.email} {`\n`} Password : {input.password}</Text>*/}
+        <TextInput
+          style={styles.input1}
+          placeholder="Enter Email"
+          value={input.email}
+          onChangeText={(email) => inputs("email", email)}
+          keyboardType="email-address"
+        />
+        <Text style={{ fontSize: 14, marginHorizontal: 20, marginVertical: -15, marginBottom: 20 }}>{emailV && "❌ Invalid Email"}</Text>
+        <TextInput
+          style={styles.input1}
+          placeholder="Enter Password"
+          value={input.password}
+          onChangeText={(password) => inputs("password", password)}
+          secureTextEntry={true}
+        />
+        <Text style={{ fontSize: 14, marginHorizontal: 20, marginVertical: -15, marginBottom: 20 }}>{active && invalid}</Text>
+        <Pressable
+          onPress={() => Alert.alert("LOGIN clicked")}
+          style={({ pressed }) => [
+            {
+              opacity: pressed ? 0.2 : 1,
+            }
+          ]}
+          disabled={active}
+        >
+          {({ pressed }) => (<Text style={{ ...styles.textPressable, backgroundColor: (!emailV && !active) ? "green" : "grey" }}>LOGIN</Text>)}
+        </Pressable>
+        <Pressable1 title={"LOGIN WITH GOOGLE"} bg={"coral"} />
+        <Pressable1 title={"LOGIN WITH FACEBOOK"} bg={"lightblue"} />
+      </ScrollView>
     </SafeAreaView>
   );
 }
@@ -112,35 +128,35 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     backgroundColor: 'teal',
-    marginTop:Platform.select({android:0, ios:0, web:0}),
-    borderWidth:1,
-    borderColor:"orange",
-    paddingTop:50,
-    padding:30,
+    marginTop: Platform.select({ android: 0, ios: 0, web: 0 }),
+    borderWidth: 1,
+    borderColor: "orange",
+    paddingTop: 50,
+    padding: 30,
   },
-  input1 :{
-    height:40,
-    borderWidth:1.5,
-    marginVertical:25,
-    borderRadius:20,
-    paddingLeft:20,
-    fontSize:20,
-    color:"white",
-    placeholderTextColor:"grey",
+  input1: {
+    height: 40,
+    borderWidth: 1.5,
+    marginVertical: 25,
+    borderRadius: 20,
+    paddingLeft: 20,
+    fontSize: 20,
+    color: "white",
+    placeholderTextColor: "grey",
   },
-  text1:{
-    fontSize:35,
-    textAlign:"center",
-    color:"white",
-    fontWeight:"800",
+  text1: {
+    fontSize: 35,
+    textAlign: "center",
+    color: "white",
+    fontWeight: "800",
   },
-  textPressable:{
-    borderWidth:1.5,
-    borderRadius:20,
-    padding:10,
-    textAlign:"center",
-    fontSize:20,
-    fontWeight:500,
-    overflow:"hidden"
+  textPressable: {
+    borderWidth: 1.5,
+    borderRadius: 20,
+    padding: 10,
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: 500,
+    overflow: "hidden"
   }
 });

28eab08294c1de9352ae37e8f954b465b918bd42
sonukryadav <sonukryadav56@gmail.com>
2023-03-03 02:23:53 +0530

    stack

--- w3/Assignmnet/StackNavigation/App.js
+++ w3/Assignmnet/StackNavigation/App.js
@@ -1,5 +1,6 @@
 import 'react-native-gesture-handler';
-import { StyleSheet, Text, View,Button } from 'react-native';
+import React, {useState, useEffect } from 'react';
+import { StyleSheet, Text, View,Button, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -9,10 +10,14 @@ const Stack = createStackNavigator();
 function HomeScreen({ navigation, route }) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen { route.params.itemId}</Text>
+      <Text>Home Screen { route.params?.itemId}</Text>
       <Button
         title="Go to Details"
         onPress={() => navigation.navigate('Details', { itemId: 86, otherParam: 'anything you want here', })}
+      />
+      <Button
+        title="PostHomeScreen"
+        onPress={() => navigation.navigate('PostHomeScreen', { itemId: 86, otherParam: 'anything you want here', })}
       />
     </View>
   );
@@ -39,12 +44,65 @@ function DetailsScreen({ route, navigation }) {
   );
 }
 
+function PostHomeScreen({ navigation, route }) {
+  console.log(route.params?.post);
+  React.useEffect(() => {
+    if (route.params?.post) {
+      // Post updated, do something with `route.params.post`
+      // For example, send the post to the server
+    }
+  }, [route.params?.post]);
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Button
+        title="Go to CreatePostScreen"
+        onPress={() => navigation.navigate('CreatePostScreen')}
+      />
+      <Button
+        title="Go to Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <Text style={{ margin: 10 }}>Post: {route.params?.post}</Text>
+    </View>
+  );
+}
+
+function CreatePostScreen({ navigation, route }) {
+  const [postText, setPostText] = React.useState('');
+
+  return (
+    <>
+      <TextInput
+        multiline
+        placeholder="What's on your mind?"
+        style={{ height: 200, padding: 10, backgroundColor: 'white' }}
+        value={postText}
+        onChangeText={setPostText}
+      />
+      <Button
+        title="Done"
+        onPress={() => {
+          // Pass and merge params back to home screen
+          navigation.navigate({
+            name: 'PostHomeScreen',
+            params: { post: postText },
+            merge: true,
+          });
+        }}
+      />
+    </>
+  );
+}
+
 
 const MyStack = () => {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Overview" }} initialParams={{ itemId: 42 }} />
+      <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Overview" }} initialParams={{itemId:100}} />
       <Stack.Screen name="Details" component={DetailsScreen} />
+      <Stack.Screen name="PostHomeScreen" component={PostHomeScreen} />
+      <Stack.Screen name="CreatePostScreen" component={CreatePostScreen} />
     </Stack.Navigator>
   );
 }

c53d097d93ab8c55d0a33a8728db688e9082e3c1
sonukryadav <sonukryadav56@gmail.com>
2023-03-09 00:46:36 +0530

    notification 1

--- w3/Assignmnet/CreateLnkedinSkeleton-Assignment/Screens/Notifications.jsx
+++ w3/Assignmnet/CreateLnkedinSkeleton-Assignment/Screens/Notifications.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator, } from '@react-navigation/native-stack';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import { View, Image, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Feather } from 'react-native-vector-icons';
 
@@ -9,19 +9,13 @@ const Stack = createNativeStackNavigator();
 const RecentConnect = () => {
     return (
         <TouchableOpacity>
-            <View style={{flex:1, flexDirection:"row", justifyContent:"space-between", backgroundColor:"black", }}>
-                <View>
-                    <Image source={{ uri: 'https://images.unsplash.com/photo-1623091410901-00e2d268901f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aW5kaWFuJTIwZ2lybHxlbnwwfHwwfHw%3D&w=1000&q=80' }}
-                        style={{width:50, height:50, borderRadius:50}}
+            <View style={styles.view1}>
+                <Image source={{ uri: 'https://images.unsplash.com/photo-1623091410901-00e2d268901f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8aW5kaWFuJTIwZ2lybHxlbnwwfHwwfHw%3D&w=1000&q=80' }}
+                        style={styles.Img1}
                     />
-                </View>
-                <View>
-                    <Text>Reach out to your recent connections <Text style={{ fontWeight: 800 }}>Anjali Mondal</Text></Text>
-                </View>
-                <View>
+                    <Text>Reach out to your recent connections <Text style={{ fontWeight: 800, }}>Anjali Mondal</Text></Text>
                     <Text style={{fontSize:19}}>12h</Text>
                     {/* <Feather name="more-vertical" /> */}
-                </View>
             </View>
         </TouchableOpacity>
     );
@@ -51,12 +45,14 @@ const ViewJobs = () => {
 
 const Notification = () => {
     return (
-        <View style={{ backgroundColor: "black" }}>
-            <RecentConnect />
-            <JobAlert />
-            <EarlyApply />
-            <ViewJobs />
-        </View>
+        <SafeAreaView>
+            <View>
+                <RecentConnect />
+                <JobAlert />
+                <EarlyApply />
+                <ViewJobs />
+            </View>
+        </SafeAreaView>
     );
 }
 
@@ -67,6 +63,20 @@ function Notifications() {
         </Stack.Navigator>
     );
 }
+
+const styles = StyleSheet.create({
+    view1: {
+        // flex: 1,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        backgroundColor: "red",
+    },
+    Img1: {
+        width: 50,
+        height: 50,
+        borderRadius: 50
+    }
+});
 
 
 export default Notifications;

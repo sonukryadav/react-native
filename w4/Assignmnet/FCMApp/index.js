8b832317713c032f0280d5fba71fec2ee76a048a
sonukryadav <sonukryadav56@gmail.com>
2023-03-25 12:17:08 +0530

    done

--- w4/Assignmnet/FCMApp/index.js
+++ w4/Assignmnet/FCMApp/index.js
@@ -1,9 +1,14 @@
-/**
- * @format
- */
+import { registerRootComponent } from 'expo';
+import messaging from '@react-native-firebase/messaging';
 
-import {AppRegistry} from 'react-native';
+// Register background handler
+messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('Message handled in the background!', remoteMessage);
+});
+
 import App from './App';
-import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also ensures that whether you load the app in Expo Go or in a native build,
+// the environment is set up appropriately
+registerRootComponent(App);
